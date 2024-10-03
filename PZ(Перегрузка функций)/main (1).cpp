0ea3aa05d0ae7e67c9a8f86487a160d98e85b9ff
Defi1403 <144542367+Defi1403@users.noreply.github.com>
2024-10-03 16:52:01 +0300

    Delete PZ(Перегрузка функций) directory

--- PZ(Перегрузка функций)/main (1).cpp
+++
@@ -1,6 +0,0 @@
-void readArray(int* arr, int size);
-void readArray(double* arr, int size);
-void printArray(int* arr, int size);
-void printArray(double* arr, int size);
-int sumArray(int* arr, int size);
-double sumArray(double* arr, int size);
