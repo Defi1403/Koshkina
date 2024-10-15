a6a643677a2c4d8d6ce4768b5483c4ee07d457a7
Defi1403 <144542367+Defi1403@users.noreply.github.com>
2024-10-15 22:28:41 +0300

    Update main (1).cpp

--- main (1).cpp
+++ main (1).cpp
@@ -4,16 +4,17 @@
 int main() {
     displayIntro();
     int currentLocation = 0;
+    int addCond = -1;
     bool gameRunning = true;
-    char move[20];
+    char move[20] = {0};
     
     
     while (gameRunning) {
         displayCurrentLocation(currentLocation);
-        displayAdditionalLocation(currentLocation);
+        displayAdditionalLocation(currentLocation, addCond);
         //std::cout << "Ваш ход (инвентарь/уйти/ответить/осмотреться/перейти мост/осмотреть пьедестал/взять реликвию/осмотреть ширму/использовать зажигалку/наступить на плиты): ";
         std::cin >> move;
-        gameRunning = processMove(move, currentLocation);
+        gameRunning = processMove(move, currentLocation, addCond);
     }
     return 0;
 }
