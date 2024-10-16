bee2583098f50356f9e7ca473bcab9af241c3b69
Defi1403 <144542367+Defi1403@users.noreply.github.com>
2024-10-16 21:36:52 +0300

    Update game.h

--- game.h
+++ game.h
@@ -1,17 +1,13 @@
-#ifndef GAME_H
-#define GAME_H
-
 const int MAX_INVENTORY_ITEMS = 4;
-const int MAX_LOCATIONS = 4;
+const int MAX_LOCATIONS = 5;
 
 extern const char* inventory[MAX_INVENTORY_ITEMS];
 extern const char* locations[MAX_LOCATIONS];
 extern int lives;
+extern int fixed;
 
 void displayIntro();
 void displayCurrentLocation(int locationIndex);
 void displayAdditionalLocation(int locationIndex, int condition);
 bool processMove(char* move, int& currentLocation, int& addCond);
 void displayInventory();
-
-#endif // GAME_H
